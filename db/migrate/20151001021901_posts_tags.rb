4fea46a1942b347a05cc9a9a6378706b1ee1a607
Askarbek Karasasev <askar.karasaev@gmail.com>
2015-10-01 11:32:24 +0900

    first commit

---
+++ db/migrate/20151001021901_posts_tags.rb
@@ -0,0 +1,18 @@
+class PostsTags < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.timestamps null: false
+    end
+
+    create_table :tags do |t|
+      t.string :name
+      t.timestamps null: false
+    end
+
+    create_table :posts_tags, id: false do |t|
+      t.belongs_to :post, index: true
+      t.belongs_to :tag, index: true
+    end
+  end
+end
